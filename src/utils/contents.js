51668d32fde4cae35e3661d86ad29454cc91b98b
Derrick Hammer <derrick@derrickhammer.com>
2022-06-12 04:03:02 -0400

    *Upgrade to support skynet and arweave

--- src/utils/contents.js
+++ src/utils/contents.js
@@ -1,7 +1,7 @@
 import contentHash from 'content-hash'
 import { ethers } from 'ethers'
 import bs58 from 'bs58'
-const supportedCodecs = ['ipns-ns', 'ipfs-ns', 'swarm-ns', 'onion', 'onion3']
+const supportedCodecs = ['ipns-ns', 'ipfs-ns', 'skynet-ns', 'swarm-ns', 'onion', 'onion3']
 
 const utils = ethers.utils
 
@@ -10,27 +10,33 @@ export function decodeContenthash(encoded) {
   if (encoded.error) {
     return { protocolType: null, decoded: encoded.error }
   }
-  if (encoded) {
-    try {
-      decoded = contentHash.decode(encoded)
-      const codec = contentHash.getCodec(encoded)
-      if (codec === 'ipfs-ns') {
-        protocolType = 'ipfs'
-      } else if (codec === 'ipns-ns') {
-        protocolType = 'ipns'
-      } else if (codec === 'swarm-ns') {
-        protocolType = 'bzz'
-      } else if (codec === 'onion') {
-        protocolType = 'onion'
-      } else if (codec === 'onion3') {
-        protocolType = 'onion3'
-      } else {
-        decoded = encoded
-      }
-    } catch (e) {
-      error = e.message
+    if (encoded) {
+        try {
+            decoded = contentHash.decode(encoded);
+
+            var codec = contentHash.getCodec(encoded);
+
+            if (codec === 'ipfs-ns') {
+                protocolType = 'ipfs';
+            } else if (codec === 'ipns-ns') {
+                protocolType = 'ipns';
+            } else if (codec === 'swarm-ns') {
+                protocolType = 'bzz';
+            } else if (codec === 'onion') {
+                protocolType = 'onion';
+            } else if (codec === 'onion3') {
+                protocolType = 'onion3';
+            } else if (codec === 'skynet-ns') {
+                protocolType = 'sia';
+            } else if (codec === 'arweave-ns') {
+                protocolType = 'arweave';
+            } else {
+                decoded = encoded;
+            }
+        } catch (e) {
+            error = e.message;
+        }
     }
-  }
   return { protocolType, decoded, error }
 }
 
@@ -55,48 +61,53 @@ export function encodeContenthash(text) {
   let encoded = false
   if (!!text) {
     let matched =
-      text.match(/^(ipfs|ipns|bzz|onion|onion3):\/\/(.*)/) ||
+      text.match(/^(ipfs|sia|ipns|bzz|onion|onion3):\/\/(.*)/) ||
       text.match(/\/(ipfs)\/(.*)/) ||
       text.match(/\/(ipns)\/(.*)/)
     if (matched) {
       contentType = matched[1]
       content = matched[2]
     }
-    try {
-      if (contentType === 'ipfs') {
-        if (content.length >= 4) {
-          encoded = '0x' + contentHash.encode('ipfs-ns', content)
-        }
-      } else if (contentType === 'ipns') {
-        let bs58content = bs58.encode(
-          Buffer.concat([
-            Buffer.from([0, content.length]),
-            Buffer.from(content),
-          ])
-        )
-        encoded = '0x' + contentHash.encode('ipns-ns', bs58content)
-      } else if (contentType === 'bzz') {
-        if (content.length >= 4) {
-          encoded = '0x' + contentHash.fromSwarm(content)
-        }
-      } else if (contentType === 'onion') {
-        if (content.length == 16) {
-          encoded = '0x' + contentHash.encode('onion', content)
-        }
-      } else if (contentType === 'onion3') {
-        if (content.length == 56) {
-          encoded = '0x' + contentHash.encode('onion3', content)
-        }
-      } else {
-        console.warn('Unsupported protocol or invalid value', {
-          contentType,
-          text,
-        })
+
+      try {
+          if (contentType === 'ipfs') {
+              if (content.length >= 4) {
+                  encoded = '0x' + _contentHash["default"].encode('ipfs-ns', content);
+              }
+          } else if (contentType === 'ipns') {
+              encoded = '0x' + _contentHash["default"].encode('ipns-ns', content);
+          } else if (contentType === 'bzz') {
+              if (content.length >= 4) {
+                  encoded = '0x' + _contentHash["default"].fromSwarm(content);
+              }
+          } else if (contentType === 'onion') {
+              if (content.length == 16) {
+                  encoded = '0x' + _contentHash["default"].encode('onion', content);
+              }
+          } else if (contentType === 'onion3') {
+              if (content.length == 56) {
+                  encoded = '0x' + _contentHash["default"].encode('onion3', content);
+              }
+          } else if (contentType === 'sia') {
+              if (content.length == 46) {
+                  encoded = '0x' + _contentHash["default"].encode('skynet-ns', content);
+              }
+          } else if (contentType === 'arweave') {
+              if (content.length == 43) {
+                  encoded = '0x' + _contentHash["default"].encode('arweave-ns', content);
+              }
+          } else {
+              console.warn('Unsupported protocol or invalid value', {
+                  contentType: contentType,
+                  text: text
+              });
+          }
+      } catch (err) {
+          console.warn('Error encoding content hash', {
+              text: text,
+              encoded: encoded
+          });
       }
-    } catch (err) {
-      console.warn('Error encoding content hash', { text, encoded })
-      //throw 'Error encoding content hash'
-    }
   }
   return encoded
 }
